895179eded4a6a36eb997fe84ab4bfd5ad36e8b5
Bruno Cintra <bruno.cintra@sovos.com>
2022-09-26 13:06:23 -0300

    adding capability or reading paths used memory

--- resourcesReader.py
+++ resourcesReader.py
@@ -1,7 +1,14 @@
+from asyncio.windows_events import NULL
 from time import sleep
 import psutil
 import math
+import os
 
+process_name = "firefox.exe"#"Code.exe"
+_paths = [ 
+          "C:\Crows\Monitoring",
+          "C:\\Logs\\2022"
+          ]
 
 def find_procs_by_name(name):
     "Return a list of processes matching 'name'."
@@ -20,19 +27,33 @@ def convert_size(size_bytes):
    s = round(size_bytes / p, 2)
    return "%s %s" % (s, size_name[i])
 
-process_name = "firefox.exe"#"Code.exe"
-
+def getUsedSpace(paths):
+    size = 0
+    totalSize = 0
+    
+    for rootPath in paths:
+        for path, dirs, files in os.walk(rootPath):
+            for f in files:
+                fp = os.path.join(path, f)
+                size += os.path.getsize(fp)
+    
+        print("\nPath ['"+path+"'] "+ convert_size(size))
+        totalSize += size
+    
+    print("\n\ntotal size ==>: "+str(convert_size(totalSize)))
+    
+    print("============================")
+    
 
 def monitorProcess(process_name):
-    while True:
-
+    
         procs = find_procs_by_name(process_name)
 
         usedCPU = 0
         usedRAM = 0
 
         if psutil.WINDOWS:
-            print("Windows OS detected")
+            print("============================\nWindows OS detected")
             
             for process in procs:
                 usedCPU += process.cpu_percent(interval=None)/10
@@ -59,9 +80,12 @@ def monitorProcess(process_name):
         else:
             print("Unknown OS")
             
-            
-        print("\n"+"CPU: "+ "{:.2f}%".format(usedCPU) + " Mem: "+ "{:.2f}%".format(usedRAM))
+        print("\nCPU: {:.2f}%".format(usedCPU) + " Mem: {:.2f}%".format(usedRAM))
 
-        sleep(5)
+while True:
 
-monitorProcess(process_name)
+    monitorProcess(process_name)
+
+    getUsedSpace(_paths)
+    
+    sleep(1)
